7ba6f2b3744350dcbf7c23727796eee032408a7f
dedelaravel15 <latihanlaravel15@gmail.com>
2023-10-08 11:40:11 +0700

    membuat membuat portofolio dengan html,css,js baru sampai about

---
+++ script.js
@@ -0,0 +1,14 @@
+const satu = document.getElementById('satu')
+const dua = document.getElementById('dua')
+const conten = document.getElementById('conten')
+
+satu.addEventListener("click", ()=>{
+    conten.innerHTML = "Saya seorang siswa kelas 12 PPLG yang sedang belajar bikin portopolio"
+    
+})
+
+dua.addEventListener('click', ()=>{
+    conten.innerHTML = "Cita cita saya membangun rumah sakit dan sekolah gratis"  
+   
+     
+})
